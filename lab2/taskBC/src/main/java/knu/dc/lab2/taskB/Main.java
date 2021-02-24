5507e0e0a1963c4d25f011c048213a6dde4b6e05
DarynaBond <35077916+WAG13@users.noreply.github.com>
2021-02-24 23:49:42 +0200

    Lab 2

--- lab2/taskBC/src/main/java/knu/dc/lab2/taskB/Main.java
+++ lab2/taskBC/src/main/java/knu/dc/lab2/taskB/Main.java
@@ -7,13 +7,13 @@ import knu.dc.lab2.taskB.handlers.PetrovHandler;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        int stolenThingsNumber = 50;
+        int stolenThingsNumber = 10;
         ProducerConsumerQueue<Integer> ivanovQueue = new ProducerConsumerQueue<>(10);
-        ProducerConsumerQueue<Integer> necheporycQueue = new ProducerConsumerQueue<>(10);
+        ProducerConsumerQueue<Integer> nechiporchukQueue = new ProducerConsumerQueue<>(10);
 
         IvanovHandler ivanovHandler = new IvanovHandler(ivanovQueue, stolenThingsNumber);
-        PetrovHandler petrovHandler = new PetrovHandler(ivanovQueue, necheporycQueue, stolenThingsNumber);
-        NechiporchukHandler nechiporchukHandler = new NechiporchukHandler(necheporycQueue, stolenThingsNumber);
+        PetrovHandler petrovHandler = new PetrovHandler(ivanovQueue, nechiporchukQueue, stolenThingsNumber);
+        NechiporchukHandler nechiporchukHandler = new NechiporchukHandler(nechiporchukQueue, stolenThingsNumber);
 
         Thread[] threads = new Thread[] {
                 new Thread(ivanovHandler),
